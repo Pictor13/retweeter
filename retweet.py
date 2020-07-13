5758041f4244cfd343d4177767627d8c469f9360
Igor Pellegrini <igor.pellegrini@live.com>
2020-07-13 15:47:14 +0200

    Lint code, add comments, fix syntax errors

--- retweet.py
+++ retweet.py
@@ -14,7 +14,8 @@ CONSUMER_SECRET = 'xxxxxxxxxxxxxxxxxxxx'
 ACCESS_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 ACCESS_SECRET = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 
-# API authentication
+# API authentication --------------------------------------
+
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(
@@ -26,8 +27,15 @@ api = tweepy.API(
     wait_on_rate_limit_notify = True,
 )
 
-# max 5 minutes rate limit, according to docs (see https://developer.twitter.com/ja/docs/basics/rate-limits)
+
+# Variables definition --------------------------------------
+
+search_text = '#spininelfianco'  # TODO: text from executable/script argument
+
+# max 5 minutes rate limit
+# See docs at: https://developer.twitter.com/ja/docs/basics/rate-limits
 api_rate_limit = 5*60
+
 # messages for retweeting
 random_messages = [
     ' esempio tweet ',
@@ -35,33 +43,40 @@ random_messages = [
     ' https://imgur.com/miOWDkq',
     ' #iGattiniOdianoSalvini https://imgur.com/0ZlAC0P'
 ]
+
 # WOEID of ROMA
 woeid = 721943
-# fetching the trends names
-trend_names = fetch_trend_names()
 
+# storage for laast id
 FILE_NAME = 'last_seen_id.txt'
 
-def fetch_trend_names()
-    api.trends_place(id = woeid)
-    trend_names = [];
+
+# Functions definition --------------------------------------
+
+def fetch_trend_names():
+    trends = api.trends_place(id=woeid)
+    trend_names = []
     for trend in trends:
         trend_names.append(trend.name)
 
-def get_random_message()
+
+def get_random_message():
     random.choice(random_messages)
 
+
 def retrieve_last_seen_id(file_name):
-    f_read = open(file_name,'r')
+    f_read = open(file_name, 'r')
     last_seen_id = int(f_read.read().strip())
     f_read.close()
     return last_seen_id
+
 
 def store_last_seen_id(last_seen_id, file_name):
     f_write = open(file_name, 'w')
     f_write.write(str(last_seen_id))
     f_write.close()
     return
+
 
 def reply():
     print("replying...")
@@ -72,13 +87,19 @@ def reply():
         print(str(mention.id) + ' - ' + mention.text)
         last_seen_id = mention.id
         store_last_seen_id(last_seen_id, FILE_NAME)
-        if '#spininelfianco' in mention.text.lower():
+        if search_text in mention.text.lower():
             print('found SpiniNelFianco! #MaryNewsWeb!')
             print('responding back...')
             api.update_status('@'+mention.user.screen_name + get_random_message(), mention.id)
             api.retweet(mention.id)
 
-# run program
+
+# run program --------------------------------------
+
+# fetching the trends names
+trend_names = fetch_trend_names()
+
+# pool
 while True:
     reply()
     time.sleep(api_rate_limit)
